b2748cc667b7527b820208a4948c189dd0618e06
aaron-baila <aaron.e.baila@gmail.com>
2022-08-03 14:24:34 +0200

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { DogsComponent } from './components/dogs/dogs.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
 
+const misRutas: Routes = [
+  // {path: 'home', component:HomeComponent}
+  { path: 'landing', component: LandingComponent },
+  { path: 'dogs', component: DogsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: '**', component: ErrorComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     DogsComponent,
     AboutUsComponent,
-    ContactComponent
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(misRutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
